6f774d2e32f7de59fa474452d62e4c6542cde3d2
Eric Oestrich <eric@oestrich.org>
2010-10-24 17:11:04 -0400

    Another room

--- main.c
+++ main.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "room.h"
 
 void link_areas(struct room *, char, struct room *);
 
 int main(){
-	struct room start;
-	struct room finish;
+	struct room * start = malloc(sizeof(struct room));
+	struct room * hallway = malloc(sizeof(struct room));
+	struct room * finish = malloc(sizeof(struct room));
 
-	start.name = "Starting area";
+	start->name = "Starting area";
 
-	finish.name = "Finish area";
+	hallway->name = "Hallway";
 
-	link_areas(&start, 'w', &finish);
+	finish->name = "Finish area";
+
+	link_areas(start, 'w', hallway);
+	link_areas(hallway, 's', finish);
 
 
 	printf("Welcome to the text game!\n");
 	printf("Your goal is to get to the finish\n");
 
-	printf("\nYou are currently in %s\n", start.name);
-	printf("To the west is %s\n", start.west->name);
+	printf("\nYou are currently in %s\n", start->name);
+	printf("To the west is %s\n", start->west->name);
+
+	free(start);
+	free(hallway);
+	free(finish);
 
 	return 0;
 }
