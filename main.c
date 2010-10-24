f6c1df4441025371b28744d586a3ed208d685e89
Eric Oestrich <eric@oestrich.org>
2010-10-24 15:50:52 -0400

    Room struct for a maze.
    Two rooms loaded in main.

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
+#include "room.h"
+
+void link_areas(struct room *, char, struct room *);
 
 int main(){
+	struct room start;
+	struct room finish;
+
+	start.name = "Starting area";
+
+	finish.name = "Finish area";
+
+	link_areas(&start, 'w', &finish);
+
+
 	printf("Welcome to the text game!\n");
+	printf("Your goal is to get to the finish\n");
+
+	printf("\nYou are currently in %s\n", start.name);
+	printf("To the west is %s\n", start.west->name);
 
 	return 0;
+}
+
+void link_areas(struct room * a, char l, struct room * b){
+	switch(l){
+		case 'n':
+			a->north = b;
+			b->south = a;
+			break;
+		case 'e':
+			a->east = b;
+			b->west = a;
+			break;
+		case 'w':
+			a->west = b;
+			b->east = a;
+			break;
+		case 's':
+			a->south = b;
+			b->north = a;
+			break;
+	}
 }
 
---
+++ room.h
@@ -0,0 +1,10 @@
+
+struct room{
+	char * name;
+
+	struct room * north;
+	struct room * east;
+	struct room * west;
+	struct room * south;
+};
+
