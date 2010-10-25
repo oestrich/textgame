8f9641ceab4e46763f9fd53e8333e1e57590dc47
Eric Oestrich <eric@oestrich.org>
2010-10-24 22:28:55 -0400

    Made a free_area() function
    Cleans up the memory for struct and sets the pointer to NULL.
    Also added comments

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include "room.h"
 
 struct room * create_area(char *);
-
+void free_area(struct room **);
 void link_areas(struct room *, char, struct room *);
 
 int main(){
@@ -20,28 +20,51 @@ int main(){
 	link_areas(start, 'w', hallway);
 	link_areas(hallway, 's', finish);
 
-
 	printf("Welcome to the text game!\n");
 	printf("Your goal is to get to the finish\n");
 
 	printf("\nYou are currently in %s\n", start->name);
 	printf("To the west is %s\n", start->west->name);
 
-	free(start);
-	free(hallway);
-	free(finish);
+	// Clear memory
+	free_area(&start);
+
+	free_area(&hallway);
+
+	free_area(&finish);
 
 	return 0;
 }
 
+// Clean up memory
+void free_area(struct room ** b){
+	free(*b);
+
+	*b = NULL;
+}
+
+// Allocate memory for a struct room
 struct room * create_area(char * name){
 	struct room * temp = malloc(sizeof(struct room));
+
+	if(temp == NULL){
+		printf("Error allocating memory\n");
+		exit(0);
+	}
 
 	temp->name = name;
 
 	return temp;
 }
 
+/** 
+ * Take two areas and link them
+ *
+ * The second argument is how b is located to a
+ * a, 'w', b goes to 
+ * a->west = b
+ * b->east = a
+ */
 void link_areas(struct room * a, char l, struct room * b){
 	switch(l){
 		case 'n':
